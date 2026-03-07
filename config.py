294cd7c1054cca7b138bba873b78cb4da1dcf064
aranea <anahita@golshani.ca>
2026-03-07 00:09:46 -0800

    shifting up and down via jk in search

--- config.py
+++ config.py
@@ -12,11 +12,16 @@ config.set('content.javascript.enabled', True, 'chrome://*/*')
 config.set('content.javascript.enabled', True, 'qute://*/*')
 config.set('content.local_content_can_access_remote_urls', True, 'file:///home/aranea/.local/share/qutebrowser/userscripts/*')
 config.set('content.local_content_can_access_file_urls', False, 'file:///home/aranea/.local/share/qutebrowser/userscripts/*')
-config.bind(",t", "config-cycle tabs.show never always")
+config.bind("t", "config-cycle tabs.show never always")
+config.bind('<Ctrl-J>', 'completion-item-focus --history next', mode='command')
+config.bind('<Ctrl-K>', 'completion-item-focus --history prev', mode='command')
+config.bind('<Ctrl-J>', 'prompt-item-focus next', mode='prompt')
+config.bind('<Ctrl-K>', 'prompt-item-focus prev', mode='prompt')
+config.bind('<Ctrl-Escape>', 'jseval -q document.activeElement && document.activeElement.blur()')
 c.editor.command = ['ghostty', '-e', 'nvim', '{file}']
-c.scrolling.bar = 'overlay'
+c.scrolling.bar = 'when-searching'
 c.scrolling.smooth = True
-c.spellcheck.languages = ["en-GB"]
+# c.spellcheck.languages = ["en-GB"]
 c.statusbar.show = 'in-mode'
 c.statusbar.position = 'bottom'
 c.tabs.position = 'left'
